7dbcdaf528a4e4e1849e0abb310b1d258dca6ca3
Guillaume HUSTA <guillaume.husta@gmail.com>
2018-06-22 15:31:25 +0200

    Add pretty print with separator for hexa string

--- src/test/java/fr/husta/test/SmartCardIOTest.java
+++ src/test/java/fr/husta/test/SmartCardIOTest.java
@@ -1,10 +1,14 @@
 package fr.husta.test;
 
+import fr.husta.test.ansi.AnsiColor;
 import org.apache.commons.codec.binary.BinaryCodec;
 import org.apache.commons.codec.binary.Hex;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.smartcardio.ATR;
 import javax.smartcardio.Card;
 import javax.smartcardio.CardChannel;
 import javax.smartcardio.CardException;
@@ -15,10 +19,14 @@ import javax.smartcardio.ResponseAPDU;
 import javax.smartcardio.TerminalFactory;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * See doc : https://doc.ubuntu-fr.org/smartcards#javaxsmartcardio
  */
 public class SmartCardIOTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SmartCardIOTest.class);
 
     @Test
     public void showListTerminals() throws CardException {
@@ -34,17 +42,16 @@ public class SmartCardIOTest {
             // filter sun.security.smartcardio.PCSCException exception
             if (cause != null &&
                     "sun.security.smartcardio.PCSCException".equals(cause.getClass().getName())) {
-                String message = e.getMessage();
+                LOGGER.debug("Exception message : {}", e.getMessage());
                 String causeMessage = cause.getMessage();
-                if ("SCARD_E_NO_READERS_AVAILABLE".equals(causeMessage)) {
-                    Assert.fail("Can't test : " + causeMessage);
-                }
+                LOGGER.debug("Exception's cause message : {}", causeMessage);
+                Assert.fail("Can't test : " + causeMessage);
             }
             throw e;
         }
 
         System.out.println("Terminals count: " + terminals.size());
-        System.out.println("Terminals: ");
+        System.out.println(AnsiColor.bold("Terminals: "));
         for (Object terminal : terminals) {
             System.out.println(" * " + terminal);
         }
@@ -57,11 +64,13 @@ public class SmartCardIOTest {
                 // Establish a connection with the card using
                 // "T=0", "T=1", "T=CL" or "*"
                 Card card = terminal.connect("*");
-                System.out.println("Card: " + card);
+                System.out.println(AnsiColor.bold("Card: (Terminal #1)"));
+                System.out.println(" * " + card);
 
                 // Get ATR (Answer-To-Reset)
-                byte[] baATR = card.getATR().getBytes();
-                System.out.println("ATR: " + toHexaString(baATR));
+                ATR cardATR = card.getATR();
+                byte[] baATR = cardATR.getBytes();
+                System.out.println("ATR: " + toHexaString(baATR, ":"));
 
                 // Select Card Manager
                 // - Establish channel to exchange APDU
@@ -94,6 +103,29 @@ public class SmartCardIOTest {
 
     public static String toHexaString(byte[] bytes) {
         return Hex.encodeHexString(bytes, false);
+    }
+
+    public static String toHexaString(byte[] bytes, String separator) {
+        if (separator == null) {
+            separator = " ";
+        }
+        String res = Hex.encodeHexString(bytes, false);
+        int nbBytes = res.length() / 2;
+        StringBuilder finalString = new StringBuilder();
+        for (int i = 0; i < nbBytes; i++) {
+            finalString.append(res, i * 2, i * 2 + 2);
+            if (i < nbBytes - 1) {
+                finalString.append(separator);
+            }
+        }
+        return finalString.toString();
+    }
+
+    @Test
+    public void testToHexaString_withSeparator() {
+        byte[] input = {(byte) 0xAB, (byte) 0xCD, (byte) 0xEF}; // AB CD EF
+        String hexaString = toHexaString(input, ":");
+        assertEquals("AB:CD:EF", hexaString);
     }
 
     public static String toBinaryString(byte[] bytes) {
